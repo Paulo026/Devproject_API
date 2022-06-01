7cc616900c7a6c2c7471da0e36bc2e1a24acdb52
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-06-01 17:55:32 -0300

    terminando a classe FuncionarioService

--- trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/service/FuncionarioService.java
+++ trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/service/FuncionarioService.java
@@ -1,6 +1,11 @@
 package org.serratec.trabalho.api.esquadrao6.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.serratec.trabalho.api.esquadrao6.dto.FuncionarioDTO;
+import org.serratec.trabalho.api.esquadrao6.exception.FuncionarioException;
 import org.serratec.trabalho.api.esquadrao6.model.Funcionario;
 import org.serratec.trabalho.api.esquadrao6.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +20,65 @@ public class FuncionarioService {
 	
 	public String salvar(FuncionarioDTO funcionarioDTO) {
 		Funcionario funcionario = new Funcionario();
+		transformarDTOEmModel(funcionario, funcionarioDTO);
+		funcionarioRepository.save(funcionario);
+		return "O funcionário foi cadastrado com uma ID: " + funcionario.getFuncionarioId();
 	}
 	
+	public FuncionarioDTO buscarPorId(Integer idFuncionario) throws FuncionarioException{
+		Optional<Funcionario> funcionario = funcionarioRepository.findById(idFuncionario);
+		Funcionario funcionarioNoBanco = new Funcionario();
+		FuncionarioDTO funcionarioDTO = new FuncionarioDTO();
+		if(funcionario.isPresent()) {
+			funcionarioNoBanco = funcionario.get();
+			transformarModelEmDTO(funcionarioNoBanco, funcionarioDTO);
+			return funcionarioDTO;
+		}
+		throw new FuncionarioException("Funcionário com a id informada, não foi encontrado"); // verificar mensagem ou melhorar
+	}
+	
+	public void deletar(Integer idFuncionario) {
+		funcionarioRepository.deleteById(idFuncionario);
+	}
+	
+	public String atualizar(Integer idFuncionario, FuncionarioDTO funcionarioDTO) throws FuncionarioException{
+		Optional<Funcionario> funcionario = funcionarioRepository.findById(idFuncionario);
+		Funcionario funcionarioBanco = new Funcionario();
+		if(funcionario.isPresent()) {
+			funcionarioBanco = funcionario.get();
+			if(funcionarioDTO.getFuncionarioCpf() != null) {
+				funcionarioBanco.setFuncionarioCpf(funcionarioDTO.getFuncionarioCpf());
+			}
+			if(funcionarioDTO.getFuncionarioNome() != null) {
+				funcionarioBanco.setFuncionarioNome(funcionarioDTO.getFuncionarioNome());
+			}
+			funcionarioRepository.save(funcionarioBanco);
+			return "O funcionario com a id " + funcionarioBanco.getFuncionarioId()+" foi atualizado";
+		}
+		throw new FuncionarioException("O funcionario não foi atualizado"); // verificar se o método está faltando algo
+	}
+	
+	public List<FuncionarioDTO> buscarTodos(){
+		List<Funcionario> funcionarioModel = funcionarioRepository.findAll();
+		List<FuncionarioDTO> listaDTO = new ArrayList<>();
+		for(Funcionario funcionario : funcionarioModel) {
+			FuncionarioDTO funcionarioDTO = new FuncionarioDTO();
+			transformarModelEmDTO(funcionario, funcionarioDTO);
+			listaDTO.add(funcionarioDTO);
+		}
+		return listaDTO;
+	}
+	
+	public void salvarListaFuncionario(List<FuncionarioDTO> listaDTO) {
+		List<Funcionario> listaFuncionario = new ArrayList<>();
+		
+		for(FuncionarioDTO funcionarioDTO : listaDTO) {
+			Funcionario funcionario = new Funcionario();
+			transformarDTOEmModel(funcionario, funcionarioDTO);
+			listaFuncionario.add(funcionario);
+		}
+		funcionarioRepository.saveAll(listaFuncionario);
+	}
 	
 	public FuncionarioDTO transformarModelEmDTO(Funcionario funcionario, FuncionarioDTO funcionarioDTO) {
 		funcionarioDTO.setFuncionarioId(funcionario.getFuncionarioId());
