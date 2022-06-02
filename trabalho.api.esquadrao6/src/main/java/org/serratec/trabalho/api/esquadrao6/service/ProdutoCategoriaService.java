6eb22b9ab20d2d561b23b70331cfdd1cb47bc7aa
Eduardo Brigatti <edubrigatti74@gmail.com>
2022-06-02 18:17:26 -0300

    atualização projeto final

--- trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/service/ProdutoCategoriaService.java
+++ trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/service/ProdutoCategoriaService.java
@@ -54,12 +54,7 @@ public class ProdutoCategoriaService {
         produtoCategoriaRepository.deleteById(categoriaId);
     }
 
-<<<<<<< HEAD
-//    public String atualizar(Integer categoriaId, ProdutoCategoriaDTO produtoCategoriaDTO) throws ProdutoCategoriaException{
-//        Optional<ProdutoCategoria> produtoCategoria = produtoCategoriaRepository.findById(categoriaId);
-//
-//    }
-=======
+
     public String atualizar(Integer categoriaId, ProdutoCategoriaDTO produtoCategoriaDTO) throws ProdutoCategoriaException{
         Optional<ProdutoCategoria> produtoCategoria = produtoCategoriaRepository.findById(categoriaId);
         ProdutoCategoria produtoCategoriaLoja = new ProdutoCategoria();
@@ -98,6 +93,5 @@ public class ProdutoCategoriaService {
         }
         produtoCategoriaRepository.saveAll(listaProdutoCategoria);
     }
->>>>>>> 94dee85f5d1f972483d82b859d0cb1801891625c
 
 }
