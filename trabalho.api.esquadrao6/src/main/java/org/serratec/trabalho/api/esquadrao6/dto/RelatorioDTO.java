a3f56e35dcaa2602e335a59b06cc0afc8d033cea
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2022-06-02 16:21:04 -0300

    Inserção no Banco de Dados

--- trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/dto/RelatorioDTO.java
+++ trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/dto/RelatorioDTO.java
@@ -2,4 +2,10 @@ package org.serratec.trabalho.api.esquadrao6.dto;
 
 public interface RelatorioDTO {
     //Colocar getters e Setters do que precisa para o relatório
+
+    public Double getValorTotal();
+
+    public Integer movimentacaoTotal();
+
+    public String produtoNome();
 }
