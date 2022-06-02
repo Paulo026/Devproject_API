e4f480a51a1a1a828da293ceb1366258002be3c5
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2022-06-02 18:57:43 -0300

    Atualização na movItem

--- trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/service/MovimentacaoItemService.java
+++ trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/service/MovimentacaoItemService.java
@@ -9,6 +9,7 @@ import org.serratec.trabalho.api.esquadrao6.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,14 +32,27 @@ public class MovimentacaoItemService {
     public MovimentacaoItemDTO buscarMovimentacaoPorID(Integer movID) {
         Optional<MovimentacaoItem> movItem = movItemRepository.findById(movID);
         MovimentacaoItem movSalva = new MovimentacaoItem();
-        MovimentacaoItemDTO DTOMovItem = new MovimentacaoItemDTO();
+        MovimentacaoItemDTO dtoMovItem = new MovimentacaoItemDTO();
 
         if (movItem.isPresent()) {
             movSalva = movItem.get();
-            movimentacaoModelDTO(movSalva, DTOMovItem);
+            movimentacaoModelDTO(movSalva, dtoMovItem);
         }
-        return DTOMovItem;
+        return dtoMovItem;
     }
+
+    public List<MovimentacaoItemDTO> buscarTodasMovimentacoes (Integer movID) {
+        List<MovimentacaoItem> listaMov = movItemRepository.findAll();
+        List<MovimentacaoItemDTO> dtoListaMov = new ArrayList<>();
+
+        for (MovimentacaoItem movItem : listaMov) {
+            MovimentacaoItemDTO dtoMovItem = new MovimentacaoItemDTO();
+            movimentacaoDTOModel(movItem, dtoMovItem);
+            dtoListaMov.add(dtoMovItem);
+        }
+        return dtoListaMov;
+    }
+
 
 
     //Relatórios
@@ -47,30 +61,30 @@ public class MovimentacaoItemService {
     }
 
     //Conversores
-    public MovimentacaoItemDTO movimentacaoModelDTO(MovimentacaoItem movItem, MovimentacaoItemDTO DTOMovItem) {
-        DTOMovItem.setMovimentacaoID(movItem.getMovimentacaoID());
-        DTOMovItem.setMovimentacaoData(movItem.getMovimentacaoData());
-        DTOMovItem.setMovimentacaoTipo(movItem.getMovimentacaoTipo());
-        DTOMovItem.setMovimentacaoQuantidade(movItem.getMovimentacaoQuantidade());
-        DTOMovItem.setMovimentacaoValorUnitario(movItem.getMovimentacaoValorUnitario());
-        DTOMovItem.setMovimentacaoNumeroDocumento(movItem.getMovimentacaoNumeroDocumento());
+    public MovimentacaoItemDTO movimentacaoModelDTO(MovimentacaoItem movItem, MovimentacaoItemDTO dtoMovItem) {
+        dtoMovItem.setMovimentacaoID(movItem.getMovimentacaoID());
+        dtoMovItem.setMovimentacaoData(movItem.getMovimentacaoData());
+        dtoMovItem.setMovimentacaoTipo(movItem.getMovimentacaoTipo());
+        dtoMovItem.setMovimentacaoQuantidade(movItem.getMovimentacaoQuantidade());
+        dtoMovItem.setMovimentacaoValorUnitario(movItem.getMovimentacaoValorUnitario());
+        dtoMovItem.setMovimentacaoNumeroDocumento(movItem.getMovimentacaoNumeroDocumento());
 
-        DTOMovItem.setClienteID(movItem.getCliente().getClienteId());
-        DTOMovItem.setProdutoID(movItem.getProduto().getProdutoId());
+        dtoMovItem.setClienteID(movItem.getCliente().getClienteId());
+        dtoMovItem.setProdutoID(movItem.getProduto().getProdutoId());
 
-        return DTOMovItem;
+        return dtoMovItem;
     }
 
-    public MovimentacaoItem movimentacaoDTOModel(MovimentacaoItem movItem, MovimentacaoItemDTO DTOMovItem) {
-        movItem.setMovimentacaoID(DTOMovItem.getMovimentacaoID());
-        movItem.setMovimentacaoData(DTOMovItem.getMovimentacaoData());
-        movItem.setMovimentacaoTipo(DTOMovItem.getMovimentacaoTipo());
-        movItem.setMovimentacaoQuantidade(DTOMovItem.getMovimentacaoQuantidade());
-        movItem.setMovimentacaoValorUnitario(DTOMovItem.getMovimentacaoValorUnitario());
-        movItem.setMovimentacaoNumeroDocumento(DTOMovItem.getMovimentacaoNumeroDocumento());
+    public MovimentacaoItem movimentacaoDTOModel(MovimentacaoItem movItem, MovimentacaoItemDTO dtoMovItem) {
+        movItem.setMovimentacaoID(dtoMovItem.getMovimentacaoID());
+        movItem.setMovimentacaoData(dtoMovItem.getMovimentacaoData());
+        movItem.setMovimentacaoTipo(dtoMovItem.getMovimentacaoTipo());
+        movItem.setMovimentacaoQuantidade(dtoMovItem.getMovimentacaoQuantidade());
+        movItem.setMovimentacaoValorUnitario(dtoMovItem.getMovimentacaoValorUnitario());
+        movItem.setMovimentacaoNumeroDocumento(dtoMovItem.getMovimentacaoNumeroDocumento());
 
-        movItem.setCliente(cliente.findById(DTOMovItem.getClienteID()).get());
-        movItem.setProduto(produto.findById(DTOMovItem.getProdutoID()).get());
+        movItem.setCliente(cliente.findById(dtoMovItem.getClienteID()).get());
+        movItem.setProduto(produto.findById(dtoMovItem.getProdutoID()).get());
 
         return movItem;
     }
