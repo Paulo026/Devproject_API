3fee850505a45ec66581653873275f0a2fce439c
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-06-06 19:48:34 -0300

    atualização do estoque

--- trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/repository/ProdutoRepository.java
+++ trabalho.api.esquadrao6/src/main/java/org/serratec/trabalho/api/esquadrao6/repository/ProdutoRepository.java
@@ -2,9 +2,23 @@ package org.serratec.trabalho.api.esquadrao6.repository;
 
 import org.serratec.trabalho.api.esquadrao6.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	
+	@Query(value="select * from produto where produto_nome = :nomeProduto limit 1",nativeQuery=true)     
+	Produto recuperarNome(String nomeProduto);
+	
+//	Produto findByprodutoNomeIgnoreCase(String produtoNome);
+	
+//	@Query(value="SELECT\r\n"
+//			+ " produto_id  \r\n"
+//			+ "FROM PRODUTO\r\n"
+//			+ " where produto_nome =\r\n"
+//			+ "" + nomeProduto + "", nativeQuery=true)
+//	Integer idPesquisado pesquisarNome();
+	
 }
